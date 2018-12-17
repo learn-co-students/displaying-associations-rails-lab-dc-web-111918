374c03006c5edf16d380f1c7daeebab9390fea13
Manon Pilaud <mandyypilaud@gmail.com>
2018-12-17 17:10:16 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,15 +1,19 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
+    @song = Song.new
   end
 
   def create
     @song = Song.new(song_params)
+    @song.artist = Artist.find_or_create_by(artist_params)
 
     if @song.save
       redirect_to @song
@@ -46,5 +50,8 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title)
   end
-end
 
+  def artist_params
+    params.require(:artist).permit(:name)
+  end
+end
